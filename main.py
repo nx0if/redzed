b40ddbe6dadcd5444e114140b86de76d0c5659ec
Weso! <55598422+nx0if@users.noreply.github.com>
2024-10-05 17:38:04 +0100

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/welcome', methods=['GET'])
 def welcome():
-    return jsonify({"status": "welcome"})
+    return jsonify({"status": "welcome to this site"})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

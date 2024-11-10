61e021bffd09d7bb07d8e4c6cb4c30a19bd029f0
Weso! <55598422+nx0if@users.noreply.github.com>
2024-11-10 21:38:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 import telebot
-
-
 
 app = Flask(__name__)
 
+# Directly use the token (be cautious with sensitive data in production)
 bot = telebot.TeleBot("7186315620:AAFVqu98eT-7ivNGaTZVxeQgaXg8NeGxM_M")
 
 @app.route('/bot_webhook', methods=['POST'])
 def bot_webhook():
-  bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
-  return 'OK'
-
+    json_data = request.get_data(as_text=True)
+    update = telebot.types.Update.de_json(json_data)
+    bot.process_new_updates([update])
+    return 'OK', 200
 
 @app.route('/set_app', methods=['GET'])
 def set_app():
-  bot.remove_webhook()
-  bot.set_webhook("https://" + request.host + "/bot_webhook")
-  return 'Done'
+    bot.remove_webhook()
+    webhook_url = "https://" + request.host + "/bot_webhook"
+    bot.set_webhook(url=webhook_url)
+    return f'Webhook set to {webhook_url}', 200
 
-
-@bot.message_handler()
+@bot.message_handler(func=lambda message: True)
 def Myfunc(message):
-  bot.send_message(message.chat.id, "Hi, What's happend?")
-
-
-
+    bot.send_message(message.chat.id, "Hi, what's happening?")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Enable threading for handling multiple requests
+    app.run(debug=True, threaded=True)
